974484254b855cebcfa8a79e5b7e84b74c6ead3c
kofi-sammy <sbee1796@gmail.com>
2024-01-23 01:54:52 +0100

    POM: Add Personal Settings page

---
+++ page-object/profile/personal-details/personal-settings.page.ts
@@ -0,0 +1,30 @@
+import {Locator, Page} from "@playwright/test";
+import MainPage from "../../main-page";
+
+/** Personal Details Page */
+class PersonalSettingsPage extends MainPage{
+  accountSetting: Locator;
+  securitySetting: Locator;
+  
+  constructor(page: Page){
+    super(page);
+    this.accountSetting = this.page.locator('a[href="/workspace/497409/settings/personal/account"]');
+    this.securitySetting = this.page.locator('a[href="/workspace/497409/settings/personal/security"]');
+  }
+
+    /**
+     * Account setting.
+     */
+    async goToAccount(): Promise<void>{
+        await this.accountSetting.click();
+    }
+
+    /**
+     * Security setting.
+     */
+    async goToUserSecuritySetting(): Promise<void>{
+        await this.securitySetting.click();
+    }
+}
+
+export default PersonalSettingsPage;
