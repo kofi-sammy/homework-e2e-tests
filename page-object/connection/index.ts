7d551e269a52c3408336402c4657c1987e66d4c6
kofi-sammy <sbee1796@gmail.com>
2024-01-25 01:22:02 +0100

    Add Landing Modale Page implementation

--- page-object/connection/index.ts
+++ page-object/connection/index.ts
@@ -1,11 +1,13 @@
 import { Page } from "@playwright/test";
 
 import UserSiginPage from "./sigin/user-signin.page";
-
+import LandingModalePage from "./landing/landing.page";
 export interface ConnectionPages {
     userSigninPage: UserSiginPage,
+    landingModalePage: LandingModalePage
 }
 
 export default(page: Page): ConnectionPages =>({
     userSigninPage: new UserSiginPage(page),
+    landingModalePage: new LandingModalePage(page),
 })
